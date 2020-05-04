82e9e028986ecb91e8c0d264a56a15c9140c850c
Brendan Reape <brendanreape88@gmail.com>
2020-05-04 09:56:06 -0700

    new accordion component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Messages from './Messages';
-import TheDate from './state/TheDate.js'
+import React from "react";
+import Tabs from './state/Tabs';
+import Accordion from "./state-drills/Accordion";
 
-class App extends Component {
+// array of objects each with a name and content
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0}/>
-        <Messages name="Notifications" unread={10}/>
-        <TheDate />
-      </div>
-    );
+    return <div>
+      <Accordion />
+    </div>
   }
 }
 
--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -1,27 +1,34 @@
 import React from 'react';
 
-constructor(props) {
+class TheDate extends React.Component {
+
+    constructor(props) {
     super(props)
     this.state = { datetime: new Date() };
     console.log('constructor')
-  }
-  componentDidMount() {
-    console.log('componentDidMount')
-    this.interval = setInterval(() => {
-      console.log('setInterval')
-      this.setState({
-        datetime: new Date()
-      })
-    }, 1000)
-  }
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-  render() {
-    console.log('render')
-    return (
-      <div>{this.state.datetime.toLocaleString()}</div>
-    )
-  }
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+        this.interval = setInterval(() => {
+        console.log('setInterval')
+        this.setState({
+            datetime: new Date()
+        })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    render() {
+        console.log('render')
+        return (
+        <div>{this.state.datetime.toLocaleString()}</div>
+        )
+    }
+
+}
 
 export default TheDate
